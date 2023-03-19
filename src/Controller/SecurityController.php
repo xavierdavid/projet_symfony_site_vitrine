5c2e0b3191cdd9a668d2dda218791f7f871187cf
Xavier DAVID <xav.david28@gmail.com>
2023-03-19 20:05:12 +0100

    Gestion de l'authentification - Création du formulaire de connexion - Routes de login et de logout

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupération d'une éventuelle erreur d'authentification
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Récupération du dernier identifiant ou email saisi dans le formulaire de login
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error
+        ]);
+    }
+}
